8d9f29fb6cc06daa62d74c3919d1b8a53672c2f3
Christopher Cahill <christopher.cahill@evanta.com>
2021-04-07 23:26:35 -0700

    getting access token

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+var whitelist = ['http://localhost:8000', 'http://localhost:8080']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+app.use(cors(corsOptions));
 
 const db = require("./bin/models");
 const Role = db.role;
